9a225319cb90fd6a4c5657d808b394ec35391936
Dave Voyles <dnvoyles@gmail.com>
2016-11-30 10:50:08 -0500

    initial commit

---
+++ ComponentLibrary/Component.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ComponentLibrary
+{
+	public class Component
+	{
+		// TODO: Replace w/ call from database
+		public string _imageUri = "https://pbs.twimg.com/profile_images/1305033822/Morbo2.jpg";
+		public string _name     = "Name: Morbo the Annihilator";
+		public string _role     = "Role: News Anchor";
+
+
+		public StackLayout personComp = new StackLayout()
+		{
+		};
+
+
+		public Label nameComp(string name)
+		{
+			Label _nameComp = new Label()
+			{
+				Text = name,
+				HorizontalTextAlignment = TextAlignment.Center,
+				TextColor = Color.Black,
+				Margin = new Thickness(0, 20, 0, 5),
+				FontSize = 18
+			};
+			return _nameComp;
+		}
+
+
+
+		public Label roleComp(string role)
+		{
+			Label _roleComp = new Label()
+			{
+				Text = role,
+				HorizontalTextAlignment = TextAlignment.Center,
+				TextColor = Color.Gray,
+				Margin = new Thickness(0, 5, 0, 5),
+				FontSize = 14
+			};
+			return _roleComp;
+		}
+
+
+		public Image photoComp(string imageUri)
+		{
+
+		Image _photo = new Image()
+		{
+			Aspect = Aspect.AspectFit,
+		    Source = ImageSource.FromUri(new Uri(imageUri))
+			//Source = ImageSource.FromFile("Img/morbo.jpg")
+			};
+			return _photo;
+		}
+
+		}
+	}
+
---
+++ ComponentLibrary/ComponentLibraryPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Xamarin.Forms;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace ComponentLibrary
+{
+	public partial class ComponentLibraryPage : ContentPage
+	{
+		// Root of page where everything will be drawn to
+		StackLayout PageRoot;
+		Component myComponent;
+		RootObject myRootObj;
+
+		public ComponentLibraryPage()
+		{
+			InitializeComponent();
+			makeWebRequest();
+			Console.WriteLine(myRootObj.name);
+
+			// Create a new component from Component.cs library
+			myComponent  = new Component();
+			var myPerson = myComponent.personComp;
+
+			// Add all of the components needed for a person
+			myPerson.Children.Add(myComponent.nameComp(myComponent._name));
+			myPerson.Children.Add(myComponent.roleComp(myComponent._role));
+			myPerson.Children.Add(myComponent.photoComp(myComponent._imageUri));
+
+
+			// Add person to the main page (DRAW IT!)
+			this.PageRoot = this.FindByName<StackLayout>("StackLayout_comp");
+			this.PageRoot.Children.Add(myPerson);
+			//this.PageRoot.Children.Add(createStarWarsComponent());
+		}
+
+
+		private View createStarWarsComponent() { 
+			myComponent  = new Component();
+			var myPerson = myComponent.personComp;
+
+			Console.WriteLine(myRootObj);
+
+			// Add all of the components needed for a person
+			myPerson.Children.Add(myComponent.nameComp(myRootObj.name));
+			myPerson.Children.Add(myComponent.roleComp(myComponent._role));
+			myPerson.Children.Add(myComponent.photoComp(myComponent._imageUri));
+
+			return myPerson;
+		}
+
+
+		/// <summary>
+		/// Retrieve JSON data from http request
+		/// </summary>
+		 public void makeWebRequest() { 
+			var client      = new RestClient("http://swapi.co/api/");
+			var request     = new RestRequest("people/1", Method.GET);
+
+			// Automatically deserialize result
+			// return content type is sniffed but can be explicitly set via RestClient.AddHandler();
+			IRestResponse<RootObject> response2 = client.Execute<RootObject>(request);
+			myRootObj = response2.Data;
+		}
+	}
+}
