13a7f876ee6c07c2d86638769fe6fdee6d9ad6dc
Dave Voyles <dnvoyles@gmail.com>
2016-11-30 11:35:04 -0500

    Added docs for components

--- ComponentLibrary/Component.cs
+++ ComponentLibrary/Component.cs
@@ -11,52 +11,69 @@ namespace ComponentLibrary
 		public string _role     = "Role: News Anchor";
 
 
-		public StackLayout personComp = new StackLayout()
+
+		/// <summary>
+		/// Empty container to hold a person's attributes: Name, Role, Image, etc.,
+		/// </summary>
+		public StackLayout personContainer = new StackLayout()
 		{
 		};
 
 
+		/// <summary>
+		/// Creates a Label based on the user's name
+		/// </summary>
+		/// <returns>Label w/ user's name</returns>
+		/// <param name="name">What is the name of this person?</param>
 		public Label nameComp(string name)
 		{
 			Label _nameComp = new Label()
 			{
-				Text = name,
+				Text 				    = name,
 				HorizontalTextAlignment = TextAlignment.Center,
-				TextColor = Color.Black,
-				Margin = new Thickness(0, 20, 0, 5),
-				FontSize = 18
+				TextColor			    = Color.Black,
+				Margin				    = new Thickness(0, 20, 0, 5),
+				FontSize 				= 18
 			};
 			return _nameComp;
 		}
 
 
-
+		/// <summary>
+		/// Creates a Label based on the user's role / position
+		/// </summary>
+		/// <returns>Label w/ user's role / position</returns>
+		/// <param name="role">What is the role / position of this person?</param>
 		public Label roleComp(string role)
 		{
 			Label _roleComp = new Label()
 			{
-				Text = role,
+				Text 					= role,
 				HorizontalTextAlignment = TextAlignment.Center,
-				TextColor = Color.Gray,
-				Margin = new Thickness(0, 5, 0, 5),
-				FontSize = 14
+				TextColor			    = Color.Gray,
+				Margin 					= new Thickness(0, 5, 0, 5),
+				FontSize 				= 14
 			};
 			return _roleComp;
 		}
 
 
+		/// <summary>
+		/// Creates a Image based on url passed in from user
+		/// </summary>
+		/// <returns>Image w/ user's mugshot</returns>
+		/// <param name="imageUri">Mugshot of user</param>
 		public Image photoComp(string imageUri)
 		{
-
-		Image _photo = new Image()
-		{
-			Aspect = Aspect.AspectFit,
-		    Source = ImageSource.FromUri(new Uri(imageUri))
-			//Source = ImageSource.FromFile("Img/morbo.jpg")
+			Image _photo = new Image() 
+			{
+				Aspect = Aspect.AspectFit,
+			    Source = ImageSource.FromUri(new Uri(imageUri))
+				//Source = ImageSource.FromFile("Img/morbo.jpg")
 			};
 			return _photo;
 		}
 
-		}
 	}
+}
 
